45f553a202d00c6946681050dee9e706e95e348a
Danitxu79 <danitxu79@users.noreply.github.com>
2019-10-26 17:14:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ NC='\033[0m'
  
 clear
 echo -e " ${LRED}####################################${NC}"
-echo -e " ${LRED}#${NC}  ${GREEN}Installing CoresUpdater v.0.8${NC}${LRED}   #${NC}"
+echo -e " ${LRED}#${NC}  ${GREEN}Installing CoresUpdater v.0.9${NC}${LRED}   #${NC}"
 echo -e " ${LRED}####################################${NC}\n"
 
 
