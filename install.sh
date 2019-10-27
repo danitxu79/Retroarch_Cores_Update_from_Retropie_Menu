375127fd8d3146b71249091dac920934133d6216
Danitxu79 <danitxu79@users.noreply.github.com>
2019-10-27 06:18:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -88,19 +88,14 @@ mv coresupdate.sh $RPMENU/coresupdate.sh
 mv coresupdate.py $RPMENU/coresupdate.py
 mv coresupdate.png $RPMENU/icons/coresupdate.png
 
-if (whoami != root)
+if [ "$EUID" -ne 0 ]
   then echo "I am not root, perfect!"
-
   else (echo "I am root, changing permissions to the files"
        chown pi $RPMENU/coresupdate.sh
        chown pi $RPMENU/coresupdate.py
        chown pi $RPMENU/icons/coresupdate.png)
 fi
 
-
-if (( $EUID != 0 )) echo 
-then{  }
-fi 
 echo -e "\n ${LRED}-${NC}${WHITE} Download complete.${NC}"
 sleep 1
 
